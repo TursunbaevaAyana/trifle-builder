f389f8463cf3599cec8a016d0eb5080568b6686f
Ayana <ayanaturrsunbaeva@gmail.com>
2020-04-14 17:42:03 +0600

    Changed the color of header

--- src/components/Layout/Logo/Logo.js
+++ src/components/Layout/Logo/Logo.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import logo from '../../../assets/logo.svg';
+import React from 'react'; 
 import classes from './Logo.module.css';
 
 export default () => (
-  <div className={classes.Logo}>
-    <img src={logo} />
+  <div className={classes.Logo}> 
     <span>Sushi Builder</span>
   </div>
 );
