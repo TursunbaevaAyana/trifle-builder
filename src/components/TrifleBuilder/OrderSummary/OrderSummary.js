9a8b35b96fb46f0ae8bda899d665558fa025b211
Ayana <ayanaturrsunbaeva@gmail.com>
2020-04-17 16:30:51 +0600

    Added Checkout button

---
+++ src/components/TrifleBuilder/OrderSummary/OrderSummary.js
@@ -0,0 +1,35 @@
+import React from "react";
+import classes from "./OrderSummary.module.css";
+import Button from "../../UI/Button/Button";
+
+const LABELS = { 
+    banana = "Banana", 
+    biscuit = "Biscuit", 
+    redVelvet =  "Red-Velvet", 
+    chocolateBiscuit = "Chocolate Biscuit", 
+    strawberry = "Strawberry", 
+    iceCream = "Ice-Cream",
+    kiwi = "Kiwi",  
+};
+
+export default ({ ingredients, cancelOrder, finishOrder, price }) => {
+  const ingredientsOutput = Object.keys(ingredients)
+    .filter((ingredient) => ingredients[ingredient] > 0)
+    .map((ingredient) => (
+      <li>
+        {LABELS[ingredient]}: {ingredients[ingredient]}
+      </li>
+    ));
+
+  return (
+    <div className={classes.OrderSummary}>
+      <h2>Your order</h2>
+      <p>Congratulations! You've built a best sushi-kit of all times!</p>
+      <ul>{ingredientsOutput}</ul>
+      <p>Total price: {price.toFixed(2)} som</p>
+      <p>Would you like to checkout?</p>
+      <Button click={finishOrder} green>Checkout</Button>
+      <Button click={cancelOrder} red>Cancel</Button>
+    </div>
+  );
+};
--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import Toolbar from './Toolbar/Toolbar';
-import Drawer from './Drawer/Drawer';
-import classes from './Layout.module.css'
+import React, { useState } from "react";
+import Toolbar from "../../components/Layout/Toolbar/Toolbar";
+import Drawer from "../../components/Layout/Drawer/Drawer";
+import classes from "./Layout.module.css";
 
-export default ({ children }) => (
-  <div className={classes.Layout}>
-    <Toolbar />
-    <Drawer />
-    {children}
-  </div>
-);
+export default ({ children }) => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  function toggleDrawer() {
+    setDrawerOpen(!drawerOpen);
+  }
+
+  return (
+    <div className={classes.Layout}>
+      <Toolbar toggleDrawer={toggleDrawer} />
+      <Drawer open={drawerOpen} toggleDrawer={toggleDrawer} />
+      <main>{children}</main>
+    </div>
+  );
+};
