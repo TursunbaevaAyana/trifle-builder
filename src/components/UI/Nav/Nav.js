caf2b061a6c9be54440932e3820cf98848f1fd37
Ayana <ayanaturrsunbaeva@gmail.com>
2020-05-18 00:33:16 +0600

    Update navigation components

---
+++ src/components/UI/Nav/Nav.js
@@ -0,0 +1,10 @@
+import React from "react";
+import NavItem from "./NavItem/NavItem";
+import classes from "./Nav.module.css";
+
+export default () => (
+  <ul className={classes.Nav}>
+    <NavItem url="/builder">Sushi Builder</NavItem>
+    <NavItem url="/orders">Orders</NavItem>
+  </ul>
+);
