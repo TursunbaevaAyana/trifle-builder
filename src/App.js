34c4cb9c44adea1a863a105bb3a119b415d4edd7
Ayana <ayanaturrsunbaeva@gmail.com>
2020-05-18 00:14:54 +0600

    Add Route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import Layout from './containers/Layout/Layout';
-import TrifleBuilder from './containers/TrifleBuilder/TrifleBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import './App.css';
-
+import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Layout from "./containers/Layout/Layout";
+import TrifleBuilder from "./containers/TrifleBuilder/TrifleBuilder";
+import Checkout from "./containers/Checkout/Checkout";
+import Orders from "./containers/Orders/Orders";
+import "./App.css";
 
 export default () => {
   return (
     <div className="App">
       <Layout>
-        <TrifleBuilder />
-        <Checkout />
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/builder" />
+          </Route>
+          <Route path="/builder">
+            <TrifleBuilder />
+          </Route>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+          <Route path="/orders">
+            <Orders />
+          </Route>
+        </Switch>
       </Layout>
     </div>
   );
