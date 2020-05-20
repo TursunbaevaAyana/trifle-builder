a711bd200d506e8268ccd71b8b3d9e85235d9e87
Ayana <ayanaturrsunbaeva@gmail.com>
2020-05-20 22:49:02 +0600

    Added some changes

--- src/components/UI/Nav/Nav.js
+++ src/components/UI/Nav/Nav.js
@@ -4,7 +4,7 @@ import classes from "./Nav.module.css";
 
 export default () => (
   <ul className={classes.Nav}>
-    <NavItem url="/builder">Sushi Builder</NavItem>
+    <NavItem url="/builder">Trifle Builder</NavItem>
     <NavItem url="/orders">Orders</NavItem>
   </ul>
 );
--- src/components/UI/Nav/NavItem/NavItem.js
+++ src/components/UI/Nav/NavItem/NavItem.js
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 import classes from "./NavItem.module.css";
 
 export default ({ children, url }) => (
-  <li className={classes.NaviItem}>
+  <li className={classes.NavItem}>
     <NavLink to={url} activeClassName={classes.active} exact>
       {children}
     </NavLink>
