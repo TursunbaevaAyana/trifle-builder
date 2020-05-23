bf73aaf84c21274aac8996f9271f48c15a6dea82
Ayana <ayanaturrsunbaeva@gmail.com>
2020-05-24 00:12:41 +0600

    Add action types

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,19 @@
+import * as actions from "./actions";
+
 const initialState = {
     ingredients: nukl,
     price: 100,
 };
 
 export default (state = initialState, action) => {
-return state;
-}
+ switch (action.type) {
+     case actions.ADD_INGREDIENT:
+        return state;
+         
+     case actions.REMOVE_INGREDIENT:
+        return state;
+
+     default:
+         return state;
+ }
+};
