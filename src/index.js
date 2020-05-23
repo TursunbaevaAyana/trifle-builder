49be25743da71e139cf9880f1c98c7ed9ae4eac1
Ayana <ayanaturrsunbaeva@gmail.com>
2020-05-24 00:00:34 +0600

    Add store and reducer

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import * as serviceWorker from './serviceWorker';
 import App from './App';
+import reducer from "./store/reducer";
 
-
+const store = createStore(reducer);
 
 ReactDOM.render(
- <BrowserRouter> 
-   <App /> 
- </BrowserRouter>,
+ <Provider store={store}>
+   <BrowserRouter> 
+    <App /> 
+  </BrowserRouter>
+ </Provider>,
  document.getElementById('root')
 );
 
---
+++ src/store/reducer.js
@@ -0,0 +1,8 @@
+const initialState = {
+    ingredients: nukl,
+    price: 100,
+};
+
+export default (state = initialState, action) => {
+return state;
+}
