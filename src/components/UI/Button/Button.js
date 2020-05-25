3331659c20fed53084f8a8e33dacb9cc2d353ab7
Ayana <ayanaturrsunbaeva@gmail.com>
2020-05-25 08:31:19 +0600

    Stylized some components

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,11 +1,11 @@
 import React from "react";
 import classes from "./Button.module.css";
 
-export default ({ children, click, enabled, red, orange }) => {
+export default ({ children, click, enabled, red, green }) => {
   const buttonClasses = [classes.Button];
 
   if (red) buttonClasses.push(classes.red);
-  if (orange) buttonClasses.push(classes.orange);
+  if (green) buttonClasses.push(classes.green);
 
   return (
     <button
