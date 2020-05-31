1177aec876712ae8c6082e4b97e554b535e05ac9
Ayana <ayanaturrsunbaeva@gmail.com>
2020-06-01 00:34:03 +0600

    Add some components

--- src/components/Layout/Toolbar/Toolbar.js
+++ src/components/Layout/Toolbar/Toolbar.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import classes from './Toolbar.module.css';
-import Logo from '../../UI/Logo/Logo';
-import Nav from '../../UI/Nav/Nav';
-import DrawerToggle from '../Drawer/DrawerToggle/DrawerToggle';
+import React from "react";
+import classes from "./Toolbar.module.css";
+import Logo from "../../UI/Logo/Logo";
+import Nav from "../../UI/Nav/Nav";
+import DrawerToggle from "../Drawer/DrawerToggle/DrawerToggle";
 
-export default ({ toggleDrawer }) =>  (
-  <div className={classes.Toolbar}> 
+export default ({ toggleDrawer }) => (
+  <div className={classes.Toolbar}>
     <Logo />
     <nav>
       <Nav />
-    </nav> 
+    </nav>
     <DrawerToggle toggleDrawer={toggleDrawer} />
   </div>
 );
