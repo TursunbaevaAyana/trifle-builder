d4896e467f44916aa58ca770820e49fecf44a095
Ayana <ayanaturrsunbaeva@gmail.com>
2020-06-01 00:18:19 +0600

    Stylized some components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore, combineReducers } from "redux";
+import { createStore } from "redux";
 import { Provider } from "react-redux";
 import * as serviceWorker from './serviceWorker';
 import App from './App';
