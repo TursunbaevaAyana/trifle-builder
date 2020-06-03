2c36f02b9828617e0b3e2f2a735aa335e82d07f9
Ayana <ayanaturrsunbaeva@gmail.com>
2020-06-03 23:35:36 +0600

    Use redux actions in auth component

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,17 +1,40 @@
-import react from "react";
+import React from "react";
+import axios from "axios";
+import { start, auth } from "../../store/actions/auth";
 import withAxios from "../../hoc/withAxios/withAxios";
-import Axios from "axios";
-import classes from "*.module.css";
+import classes from "./Auth.module.css";
+import Button from "../UI/Button/Button";
+import Spinner from "../UI/Spinner/Spinner";
+import { useDispatch, useSelector } from "react-redux";
 
 export default withAxios(() => {
-    return(
-        <div className={classes.Auth}>
-            <form>
-                <h1>Sign up</h1>
-                <input type="text placeholder="E-mail name="email"></input>
-                <input type="password" placeholder="Password" name="password" required minLength="8"></input>
-                <Button green>Submit</Button>
-            </form>
-        </div>
+  const dispatch = useDispatch();
+  const { loading } = useSelector(state => state.auth);
+
+  const formSubmitted = (event) => {
+    start(dispatch);
+
+    const data = new FormData(event.target);
+    auth(dispatch, data.get('email'), data.get('password'));
+
+    event.preventDefault();
+  }
+
+  let formOutput = <Spinner />;
+  if (!loading) {
+    formOutput = (
+      <form onSubmit={formSubmitted}>
+        <h1>Welcome</h1>
+        <input type="email" placeholder="E-mail" name="email" required />
+        <input type="password" placeholder="Password" name="password" required minLength="6" />
+        <Button green>Submit</Button>
+      </form>
     );
-}, Axios);
+  }
+
+  return (
+    <div className={classes.Auth}>
+      {formOutput}
+    </div>
+  );
+}, axios);
