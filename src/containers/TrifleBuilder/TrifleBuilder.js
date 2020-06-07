1ebdababfef2e50efe59563ffe4e061c91af4ce8
Ayana <ayanaturrsunbaeva@gmail.com>
2020-06-07 23:19:06 +0600

    Add some code

--- src/containers/TrifleBuilder/TrifleBuilder.js
+++ src/containers/TrifleBuilder/TrifleBuilder.js
@@ -13,6 +13,7 @@ import classes from "./TrifleBuilder.module.css";
 
 export default withAxios(() => {
   const { ingredients, price } = useSelector(state => state.builder);
+  const isAuthenticated = useSelector(state => state.auth.token !== null);
   const [isOrdering, setIsOrdering] = useState(false);
   const history = useHistory();
   const dispatch = useDispatch();
@@ -20,6 +21,15 @@ export default withAxios(() => {
   useEffect(() => {
     load(dispatch);
   }, [dispatch]);
+
+  function startOrder() {
+    if (isAuthenticated) {
+      setIsOrdering(true);
+    }
+    else {
+      history.push('/auth?checkout');
+    }
+  }
 
   let output = <Spinner />;
   if (ingredients) {
@@ -31,7 +41,7 @@ export default withAxios(() => {
       <>
         <TrifleKit price={price} ingredients={ingredients} />
         <TrifleControls
-          startOrder={() => setIsOrdering(true)}
+          startOrder={startOrder}
           canOrder={canOrder}
           ingredients={ingredients}
         />
