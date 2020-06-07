ea75f666a2bf415965fe4677fb606655cd7c6116
Ayana <ayanaturrsunbaeva@gmail.com>
2020-06-07 21:42:06 +0600

    Use timeout on session

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -9,14 +9,23 @@ export const success = (dispatch, { idToken, localId }) => dispatch({
   type: AUTH_SUCCESS, id: idToken, token: localId
 });
 
-export const fail = (dispatch, error) => dispatch({
+export const fail = (dispatch) => dispatch({
   type: AUTH_FAIL, error
 });
+
+export const logout = (dispatch, error) => dispatch({
+  type: AUTH_LOGOUT
+});
+
+export const timeout = (dispatch, seconds) => setTimeout(() => logout(dispatch), seconds * 1000);
 
 const key = "AIzaSyCSdxi9dcNkNzF7oOjNTzTAqXAkP3g0DJo";
 const signInUrl = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" + key;
 const signUpUrl = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + key ;
 export const auth = (dispatch, method, email, password) => axios
   .post(method === "signin" ? signInUrl : signUpUrl, { email, password, returnSecureToke: true })
-  .then(({ data }) => success(dispatch, data))
+  .then(({ data }) => {
+     success(dispatch, data);
+     timeout(dispatch, +data.expiresIn);
+})
   .catch(error => fail(dispatch, error));
