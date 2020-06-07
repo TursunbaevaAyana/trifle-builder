ddc12ca1a149931605bf762360f2f00a2ea3bcc8
Ayana <ayanaturrsunbaeva@gmail.com>
2020-06-08 00:01:50 +0600

    Add logout link

--- src/App.js
+++ src/App.js
@@ -1,34 +1,65 @@
-  
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { restore } from "./store/actions/auth";
 import Layout from "./containers/Layout/Layout";
 import TrifleBuilder from "./containers/TrifleBuilder/TrifleBuilder";
 import Checkout from "./components/Checkout/Checkout";
 import Orders from "./components/Orders/Orders";
 import Auth from "./components/Auth/Auth";
+import Logout from "./components/Logout/Logout";
 import "./App.css";
 
 export default () => {
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector(state => state.auth.token !== null);
+  useEffect(() => {
+    restore(dispatch);
+  }, [dispatch]);
+
+  let routesOutput = (
+    <Switch>
+      <Route path="/" exact>
+        <Redirect to="/builder" />
+      </Route>
+      <Route path="/builder">
+        <TrifleBuilder />
+      </Route>
+      <Route path="/auth">
+        <Auth />
+      </Route>
+      <Redirect to="/" />
+    </Switch>
+  );
+  if (isAuthenticated) {
+    routesOutput = (
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/builder" />
+        </Route>
+        <Route path="/builder">
+          <TrifleBuilder />
+        </Route>
+        <Route path="/checkout">
+          <Checkout />
+        </Route>
+        <Route path="/orders">
+          <Orders />
+        </Route>
+        <Route path="/auth">
+          <Auth />
+        </Route>
+        <Route path="/logout">
+          <Logout />
+        </Route>
+      </Switch>
+    );
+  }
+
   return (
     <div className="App">
       <Layout>
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/builder" />
-          </Route>
-          <Route path="/builder">
-            <TrifleBuilder />
-          </Route>
-          <Route path="/checkout">
-            <Checkout />
-          </Route>
-          <Route path="/orders">
-            <Orders />
-          </Route>
-          <Route path="/auth">
-            <Auth />
-          </Route>
-        </Switch>
+        {routesOutput}
       </Layout>
     </div>
   );
